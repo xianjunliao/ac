9f8b2d332317a2abee9e571d2c2efceef82e2f27
xianjunliao <xianjun_liao@126.com>
2017-02-06 19:26:49 +0800

    2017020605

---
+++ src/com/ac/dao/jdbc/JdbcDao.java
@@ -0,0 +1,12 @@
+package com.ac.dao.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public interface JdbcDao {
+	 public List<Map<String,Object>> queryBySql(String sql);
+	 public List<Map<String,Object>> queryBySql(String sql,Object[]params);
+	 public JdbcTemplate getJdbcTemplate();
+}
